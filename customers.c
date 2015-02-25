6f74722104702c7de40fa73bc0bf7a62fe9a9947
Luuk Veenis <luukveenis@shaw.ca>
2015-02-24 22:34:38 -0800

    Add basic customer file parsing
    Read the file line by line and then build a customer struct
    corresponding to each line.

--- customers.c
+++ customers.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "customers.h"
+
+#define BUF_SIZE 80
+
+customer* initialize_customer(){
+  customer *c = (customer*) malloc(sizeof(customer));
+  c->num = 0;
+  c->arrive = 0;
+  c->service = 0;
+  c->priority = 0;
+
+  return c;
+}
+
+void build_customer(customer *c, char *line){
+  c->num = atoi(strtok(line, ":"));
+  c->arrive = atoi(strtok(NULL, ","));
+  c->service = atoi(strtok(NULL, ","));
+  c->priority = atoi(strtok(NULL, "\n"));
+}
 
 void process_customers(FILE *fin){
-  char buf[100];
-  int len;
+  char buf[BUF_SIZE];
+  int i,count;
 
-  while (fgets(buf, 100, fin)){
-    len = strlen(buf);
-    if (buf[len-1] == '\n') buf[len-1] = '\0';
-    printf("%s\n", buf);
+  fgets(buf, BUF_SIZE, fin);
+  for (i = 0, count = atoi(buf) ; i < count; i++){
+    fgets(buf, BUF_SIZE, fin);
+    customer *c = initialize_customer();
+    build_customer(c, buf);
   }
 }
