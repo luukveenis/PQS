3986c18ffb04dfe2f254d0fc5880190e222ae928
Luuk Veenis <luukveenis@shaw.ca>
2015-02-24 19:00:14 -0800

    Read file and simply print customers for now

--- pqs.c
+++ pqs.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void process_customers(FILE *fin){
+  char buf[100];
+  int len;
+
+  while (fgets(buf, 100, fin)){
+    len = strlen(buf);
+    if (buf[len-1] == '\n') buf[len-1] = '\0';
+    printf("%s\n", buf);
+  }
+}
 
 int main (int argc, char **argv){
   FILE *fin;
@@ -9,7 +21,8 @@ int main (int argc, char **argv){
     printf("You must specify exactly ONE input file!\n");
     exit(EXIT_FAILURE);
   }
-  fin = fopen(argv[0], "r");
+  fin = fopen(argv[1], "r");
+  process_customers(fin);
 
   return 0;
 }
