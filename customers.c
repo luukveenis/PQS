f79cc3c9dfe08fb93de27eebc0d5f0a4af7f2d32
Luuk Veenis <luukveenis@shaw.ca>
2015-02-26 11:40:07 -0800

    Add comments and clean up long lines

--- customers.c
+++ customers.c
@@ -9,10 +9,11 @@
 #include "customers.h"
 #include "customer_queue.h"
 
-#define BUF_SIZE 80
-#define BASE_TEN 10
-#define SCALING_FACTOR 10000
+#define BUF_SIZE 80 /* Buffer size to read lines from input file */
+#define BASE_TEN 10 /* Used in converting strings to ints */
+#define SCALING_FACTOR 10000 /* Tenths of a second to microseconds */
 
+/* Initializes a blank customer struct */
 customer* initialize_customer(){
   customer *c = (customer*) malloc(sizeof(customer));
   c->num = 0;
@@ -23,6 +24,9 @@ customer* initialize_customer(){
   return c;
 }
 
+/* Parses a line from the input file and assigns the values to customer c */
+/* According to the man pages, atoi has been deprecated and this use of
+ * strtol is the recommended alternative */
 void build_customer(customer *c, char *line){
   c->num = (int)strtol(strtok(line, ":"), (char**)NULL, BASE_TEN);
   c->arrive = (int)strtol(strtok(NULL, ","), (char**)NULL, BASE_TEN);
@@ -30,14 +34,17 @@ void build_customer(customer *c, char *line){
   c->priority = (int)strtol(strtok(NULL, "\n"), (char**)NULL, BASE_TEN);
 }
 
+/* Main control function executed by each customer thread */
 void* thread_control(void *ptr){
   customer *c = (customer*) ptr;
   usleep(c->arrive * SCALING_FACTOR);
-  printf("customer %2d arrives: arrival time (%.2d), service time (%.2d), priority (%2d). \n", c->num, c->arrive, c->service, c->priority);
+  printf("customer %2d arrives: arrival time (%.2d), service time (%.2d), "
+         "priority (%2d). \n", c->num, c->arrive, c->service, c->priority);
 
   return (void*)0;
 }
 
+/* Process the input file provided from the command line */
 void process_customers(FILE *fin){
   char buf[BUF_SIZE];
   int i,count;
